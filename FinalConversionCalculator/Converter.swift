73b59cd5eb5bb1b14a9d866877dcf479f37bd0d4
Zach Swartz <zachswartz@Zachs-MacBook-Pro.local>
2019-05-10 11:03:23 -0500

    Final Commit

---
+++ FinalConversionCalculator/Converter.swift
@@ -0,0 +1,16 @@
+//
+//  Converter.swift
+//  FinalConversionCalculator
+//
+//  Created by Zach Swartz on 5/10/19.
+//  Copyright © 2019 Zach Swartz. All rights reserved.
+//
+
+import Foundation
+
+struct Converter{
+    var label: String
+    var inputUnit: String
+    var outputUnit: String
+    
+}
